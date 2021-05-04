4e1d611e82c43220829630d8420b3af199fd46c6
nika-gelashvili <nika.gelash@gmail.com>
2021-05-04 17:44:08 +0400

    Define routes for api endpoints

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+
     return $request->user();
 });
+
+Route::prefix('cart')->group(function () {
+    Route::post('/add-product-in-cart', 'App\Http\Controllers\CartController@addProductInCart');
+    Route::post('/remove-product-from-cart', 'App\Http\Controllers\CartController@removeProductFromCart');
+    Route::post('/set-cart-product-quantity', 'App\Http\Controllers\CartController@setCartProductQuantity');
+    Route::get('/get-user-cart', 'App\Http\Controllers\CartController@getUserCart');
+});
+
